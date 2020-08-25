e7e2eec23c76ea54350121007031ce1144e0b04c
James Martin <james.martin@bugsnag.com>
2020-08-25 09:40:01 +0100

    added bugsnag and release stage env

--- main.go
+++ main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/bugsnag/bugsnag-go"
+	bugsnaggin "github.com/bugsnag/bugsnag-go/gin"
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
 )
 
 func main() {
 	viper.AddConfigPath(".")
+	viper.AddConfigPath("/app")
 	viper.AddConfigPath("/etc/promalert")
 	viper.SetConfigName("config")
 
@@ -16,7 +20,19 @@ func main() {
 		panic(fmt.Errorf("Fatal error config file: %s \n", err))
 	}
 	viper.AutomaticEnv()
+	viper.SetDefault("bugsnag_release_stage", "development")
 	viper.SetEnvPrefix("promalert")
+
+	bugsnag.Configure(bugsnag.Configuration{
+		APIKey: "a0936975687e3d8abbf8a593dc646f87",
+		// The import paths for the Go packages containing your source files
+		ProjectPackages: []string{"main", "github.com/bugsnag/promalert"},
+		ReleaseStage:    viper.GetString("bugsnag_release_stage"),
+		Synchronous:     true,
+	})
+
+	g := gin.Default()
+	g.Use(bugsnaggin.AutoNotify())
 
 	r := gin.New()
 	r.Use(gin.LoggerWithWriter(gin.DefaultWriter, "/healthz"))
