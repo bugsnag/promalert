e244e32a7385099a08da23057f7d1d3129fc265d
zombar <james.martin@bugsnag.com>
2021-11-03 12:38:39 +0000

    use clog and bugsnag for http_handlers.go and remove commented out code

--- http_handlers.go
+++ http_handlers.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"net/http/httputil"
 
+	"github.com/bugsnag/bugsnag-go/v2"
+	"github.com/bugsnag/microkit/clog"
 	"github.com/gin-gonic/gin"
+	"github.com/pkg/errors"
 	"github.com/spf13/viper"
 )
 
@@ -20,15 +21,16 @@ func webhook(c *gin.Context) {
 		// Save a copy of this request for debugging.
 		requestDump, err := httputil.DumpRequest(c.Request, true)
 		if err != nil {
-			fmt.Println(err)
+			err = errors.Wrap(err, "Error dumping request")
+			_ = bugsnag.Notify(err)
+			clog.Error(err)
 		}
-		log.Printf("New request")
-		fmt.Println(string(requestDump))
+		clog.Debug("New request: %s", string(requestDump))
 	}
 
 	var m HookMessage
 	if c.ShouldBindJSON(&m) == nil {
-		log.Printf("Alerts: GroupLabels=%v, CommonLabels=%v", m.GroupLabels, m.CommonLabels)
+		clog.Info("Alerts: GroupLabels=%v, CommonLabels=%v", m.GroupLabels, m.CommonLabels)
 
 		for _, alert := range m.Alerts {
 			if prevAlert, founded := FindAlert(alert); founded {
@@ -47,7 +49,7 @@ func webhook(c *gin.Context) {
 					AddAlert(alert)
 				}
 
-				log.Printf("Slack update sended, channel: %s thread: %s", respChannel, respTimestamp)
+				clog.Info("Slack update sended, channel: %s thread: %s", respChannel, respTimestamp)
 			} else {
 				// override channel if specified in rule
 				if m.CommonLabels["channel"] != "" {
@@ -74,55 +76,3 @@ func webhook(c *gin.Context) {
 
 	c.JSON(400, map[string]string{"status": "Invalid body of request"})
 }
-
-//
-//func webhook(w http.ResponseWriter, r *http.Request) {
-//	if viper.GetBool("debug") {
-//		// Save a copy of this request for debugging.
-//		requestDump, err := httputil.DumpRequest(r, true)
-//		if err != nil {
-//			fmt.Println(err)
-//		}
-//		log.Printf("New request")
-//		fmt.Println(string(requestDump))
-//	}
-//
-//	dec := json.NewDecoder(r.Body)
-//	defer r.Body.Close()
-//
-//	var m HookMessage
-//	if err := dec.Decode(&m); err != nil {
-//		log.Printf("error decoding message: %v", err)
-//		http.Error(w, "invalid request body", 400)
-//		return
-//	}
-//
-//	log.Printf("Alerts: GroupLabels=%v, CommonLabels=%v", m.GroupLabels, m.CommonLabels)
-//
-//	for _, alert := range m.Alerts {
-//		if prevAlert, founded := FindAlert(alert); founded {
-//			alert.Channel = prevAlert.Channel
-//			alert.MessageTS = prevAlert.MessageTS
-//			alert.MessageBody = prevAlert.MessageBody
-//			respChannel, respTimestamp, _ := alert.PostMessage()
-//			if alert.Status == AlertStatusFiring {
-//				alert.MessageTS = respTimestamp
-//				alert.Channel = respChannel
-//				AddAlert(alert)
-//			}
-//			log.Printf("Slack update sended, channel: %s thread: %s", respChannel, respTimestamp)
-//		} else {
-//			// post new message
-//			respChannel, respTimestamp, messageBody := alert.PostMessage()
-//			alert.MessageTS = respTimestamp
-//			alert.Channel = respChannel
-//			alert.MessageBody = messageBody
-//
-//			AddAlert(alert)
-//		}
-//	}
-//
-//	w.Header().Set("Content-Type", "application/json")
-//	_, err := w.Write([]byte("{\"success\": true}"))
-//	fatal(err, "failed to send response")
-//}
